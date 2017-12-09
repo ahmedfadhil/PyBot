9ba9bc32c6269cd16e7575aae129f906cff335fe
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-09 14:00:46 +0100

    cleaning data

--- chatbot.py
+++ chatbot.py
@@ -14,10 +14,37 @@ def create_table():
 unix INT, score INT)""")
 
 
+def format_data(data):
+    data = data.replace("\n", "newlinechar").replace("\r", "newlinechar").replace('"', "'")
+    return data
+
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print("find_parent", e)
+        return False
+
+
 if __name__ == "__manin__":
     create_table()
 
     row_counter = 0
     paired_row = 0
-with open("add your link here".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
-    pass
+    with open("add your link here".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+        # pass
+        for row in f:
+            row_counter += 1
+            row = json.load(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
